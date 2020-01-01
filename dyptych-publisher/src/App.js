c8de9e4f013e4ec900105945320db6b3b793dbe1
Nelson Alexander <alexbnelson@yahoo,com>
2020-01-01 22:46:41 +0000

    initial styling and adding bootstrap

--- dyptych-publisher/src/App.js
+++ dyptych-publisher/src/App.js
@@ -1,23 +1,16 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import './Button.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <span><h2 className="App-title">Dyptych | Publisher</h2></span>
+        <span><button type="button" className="btn-italics">Italics</button></span>
+        <span><button type="button" className="btn-iLink">Image Link</button></span>
+        
       </header>
     </div>
   );
