3cd079fba2d071e09c64b23ed0e3d59b566c8010
Nelson Alexander <alexbnelson@yahoo,com>
2020-01-02 15:13:05 +0000

    Added component for text input area

--- dyptych-publisher/src/App.js
+++ dyptych-publisher/src/App.js
@@ -3,20 +3,21 @@ import logo from './logo.svg';
 import './App.css';
 import './Button.css';
 
-function App() {
-  return (
-    <div className="App container">
-      <header className="App-header row">
-        <div className="col-sm App-title"><h2 className="">Dyptych | Publisher</h2></div>
-        <div className="col-sm button-container"><button type="button" className="btn-italics">Italics</button></div>
-        <div className="col-sm button-container"><button type="button" className="btn-iLink">Image Link</button></div>
-      </header>
-      //https://reactjs.org/docs/forms.html -Text input in react reduz
-      <div className="row textarea-row">
-        <div className='textarea' contenteditable></div>
+class App extends Component {
+  render() {
+    return (
+      <div className="App container">
+        <header className="App-header row">
+          <div className="col-sm App-title"><h2 className="">Dyptych | Publisher</h2></div>
+          <div className="col-sm button-container"><button type="button" className="btn-italics">Italics</button></div>
+         <div className="col-sm button-container"><button type="button" className="btn-iLink">Image Link</button></div>
+       </header>
+        //https://reactjs.org/docs/forms.html -Text input in react reduz
+        <div className="row textarea-row">
+          <textarea value={this.state.value} onChange={this.handleChange} />v>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
-
 export default App;
---
+++ dyptych-publisher/src/components/InputArea.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import Bootstrap from 'bootstrap';
+
+class InputArea extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+          title: 'Exekias, from craftsman to fine artist',
+          body: 'Please write an essay about your favorite DOM element.'
+        };
+    
+        this.handleBodyChange = this.handleBodyChange.bind(this);
+        this.handleTitleChange = this.handleTitleChange.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+      }
+    
+      handleBodyChange(event) {
+        this.setState({body: event.target.value});
+      }
+
+      handleTitleChange(event) {
+        this.setState({title: event.target.value});
+      }
+    
+      handleSubmit(event) {
+        alert('An essay was submitted: ' + this.state.value);
+        event.preventDefault();
+      }
+    
+      render() {
+        return (
+          <form onSubmit={this.handleSubmit}>
+            <textarea value={this.state.title} onChange={this.handleTitleChange} />
+            
+            <label>
+              Essay:
+              <textarea value={this.state.body} onChange={this.handleBodyChange} />
+            </label>
+            <input type="submit" value="Submit" />
+          </form>
+        );
+      }
+    }
+
+export default InputArea;
