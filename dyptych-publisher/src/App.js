085779307b8679a32b87654a908630d5d646c0e3
Nelson Alexander <alexbnelson@yahoo,com>
2020-01-02 11:42:52 +0000

    Added redux

--- dyptych-publisher/src/App.js
+++ dyptych-publisher/src/App.js
@@ -5,13 +5,16 @@ import './Button.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <span><h2 className="App-title">Dyptych | Publisher</h2></span>
-        <span><button type="button" className="btn-italics">Italics</button></span>
-        <span><button type="button" className="btn-iLink">Image Link</button></span>
-        
+    <div className="App container">
+      <header className="App-header row">
+        <div className="col-sm App-title"><h2 className="">Dyptych | Publisher</h2></div>
+        <div className="col-sm button-container"><button type="button" className="btn-italics">Italics</button></div>
+        <div className="col-sm button-container"><button type="button" className="btn-iLink">Image Link</button></div>
       </header>
+      //https://reactjs.org/docs/forms.html -Text input in react reduz
+      <div className="row textarea-row">
+        <div className='textarea' contenteditable></div>
+      </div>
     </div>
   );
 }
